5934f7284d5dc08cf2abfbc68c851f4e417e7ceb
MarkusPTKTK <markus.peters@tktk.ee>
2024-11-20 15:54:55 +0200

    add week 6 code

---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,28 @@
+﻿string hello = "hello, world! Here I am!";
+
+int stringLenght = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+
+hello = hello.Trim();
+
+char firstletter = hello[0];
+Console.WriteLine(firstletter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0]));
+
+firstletter = hello[0];
+Console.WriteLine(firstletter);
+
+
+//int wordCounter = 1;
+//for(int i = 0; i < hello.Lenght; i++)
+//{
+//      if (char.IsWhiteSpace(hello[i]))
+//      {
+//          wordCounter++;
+//      }
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
